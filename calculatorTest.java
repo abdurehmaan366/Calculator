b4841e2b062bfd33e973ff27706ccc55c8ba3de5
abdurehmaan366 <abdurehmaan366@gmail.com>
2025-11-05 09:39:04 -0800

    Created a calculator class and added a summ method

--- calculatorTest.java
+++ calculatorTest.java
@@ -1,3 +1,9 @@
+class calculator {
+    public int sum (int a, int b) {
+        return a + b;
+    }
+}
+
 public class calculatorTest {
     public static void main(String[] args) {
         System.out.println("My Calculator (My first github trial)");
