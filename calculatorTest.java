c4e2963769d5d0caa20906bca114f205413bed1e
abdurehmaan366 <abdurehmaan366@gmail.com>
2025-11-05 09:44:41 -0800

    Added class variables, and overloaded constructors

--- calculatorTest.java
+++ calculatorTest.java
@@ -1,6 +1,33 @@
 class calculator {
+
+    int a;
+    int b;
+
+    // no args constructor
+    public calculator () {
+
+    }
+
+    // 2 args constructor
+    public calculator (int cpy_a, int cpy_b) {
+        a = cpy_a;
+        b = cpy_b;
+    }
+
     public int sum (int a, int b) {
         return a + b;
+    }
+
+    public int subtraction (int a, int b) {
+        return a - b;
+    }
+
+    public int product (int a, int b) {
+        return a * b;
+    }
+
+    public int division (int a, int b) {
+        return a / b;
     }
 }
 
